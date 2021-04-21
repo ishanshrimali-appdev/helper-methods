681d70c4ea1c8c9b1cf7005ad80884db2bd73fee
ishanshrimali <ishanshrimali@chicagobooth.edu>
2021-04-21 01:00:41 +0000

    Replaced new form with form_with

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -34,7 +34,7 @@ class MoviesController < ApplicationController
       @the_movie.save
       redirect_to movies_url, notice: "Movie created successfully."
     else
-      render template: "new"
+      render "movies/new"
     end
   end
 
